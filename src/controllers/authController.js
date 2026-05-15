c254b7afec2c2a1d81f6dbdab4c95eeeb0737fd3
NataliaTols <kopylova9624@gmail.com>
2026-05-15 12:59:08 +0200

    first commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,92 @@
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import bcrypt from 'bcrypt';
+import { createSession, setSessionCookies } from '../services/auth.js';
+import { Session } from "../models/session.js";
+
+
+
+//      -----------REGISTER-----------
+export const registerUser = async (req, res) => {
+ const { email, password } = req.body;
+
+ const existingUser = await User.findOne({ email });
+  if (existingUser) {
+      throw createHttpError(400, 'Email in use');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+     const newUser = await User.create({
+    email,
+    password: hashedPassword,
+     });
+    
+     const newSession = await createSession(newUser._id);
+    setSessionCookies(res, newSession);
+    
+    res.status(201).json({newUser });
+};
+
+//      -----------LOGIN-----------
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+    }
+      const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+    }
+     await Session.deleteOne({ userId: user._id });
+
+
+  const newSession = await createSession(user._id);
+    setSessionCookies(res, newSession);
+    
+  res.status(200).json(user);
+};
+
+// ---------- REFRESH SESSION ----------
+export const refreshUserSession = async (req, res) => {
+  const { sessionId, refreshToken } = req.cookies;
+
+
+  if (!sessionId || !refreshToken) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+
+  const session = await Session.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+
+  const isRefreshTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+
+  if (isRefreshTokenExpired) {
+    throw createHttpError(401, 'Session token expired');
+  }
+
+
+  await Session.deleteOne({ _id: sessionId });
+
+
+  const newSession = await createSession(session.userId);
+
+
+  setSessionCookies(res, newSession);
+
+
+  res.status(200).json({
+    message: 'Session refreshed',
+  });
+};
