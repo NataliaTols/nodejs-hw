0dd343dae7f2abb5b14a8dce75f4c57047e0e65b
NataliaTols <kopylova9624@gmail.com>
2026-05-31 22:59:11 +0200

    update server

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
